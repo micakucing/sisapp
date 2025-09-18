2565103bf0c1bfa84ccdadb5a5ae213fb012fe24
micakucing <ramahardiansidik@gmail.com>
2025-09-18 13:03:14 +0700

    l

---
+++ not-found.js
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "80vh" }}>
+      <h1 className="text-danger">404</h1>
+      <p className="text-muted">Halaman tidak ditemukan</p>
+    </div>
+  );
+}
