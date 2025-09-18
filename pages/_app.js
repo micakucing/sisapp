64dc55001745fed9eb59371ce4ae3dadeff376cf
micakucing <ramahardiansidik@gmail.com>
2025-09-18 13:06:01 +0700

    l

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from '../context/AuthContext';
 import Layout from '../components/Layout';
-
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
